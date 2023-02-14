30fe26db706b39ac19ac72e952fed352ae58fb8b
Max Wood <bridges.wood@gmail.com>
2023-02-14 15:56:43 +0000

    Add display logic

--- task1.py
+++ task1.py
@@ -46,18 +46,43 @@ def main(image_path):
     for image in load_images(image_path):
         # Perform hough transform on the image
         h, theta, d = hough_transform(image)
-        
-        # Plot the hough transform
-        fig, ax = plt.subplots(1, 3, figsize=(15, 6))
+
+        fig, axes = plt.subplots(1, 3, figsize=(15, 6))
+        ax = axes.ravel()
+
         ax[0].imshow(image, cmap=plt.cm.gray)
         ax[0].set_title('Input image')
+        ax[0].set_axis_off()
+        
+        angle_step = 0.5 * np.diff(theta).mean()
+        dist_step = 0.5 * np.diff(d).mean()
+        bounds = [
+            np.rad2deg(theta[0] - angle_step),
+            np.rad2deg(theta[-1] + angle_step),
+            d[-1] + dist_step,
+            d[0] - dist_step,
+        ]
+        ax[1].imshow(np.log(1 + h), extent=bounds, cmap='gray', aspect='auto')
+        ax[1].set_title('Hough transform')
+        ax[1].set_xlabel('Angles (degrees)')
+        ax[1].set_ylabel('Distance (pixels)')
+        ax[1].axis('image')
+
+        ax[2].imshow(image, cmap=plt.cm.gray)
+        ax[2].set_ylim((image.shape[0], 0))
+        ax[2].set_axis_off()
+        ax[2].set_title('Detected lines')
+
 
         lines = []
         for _, angle, dist in zip(*hough_line_peaks(h, theta, d)):
             (x0, y0) = np.array([np.cos(angle), np.sin(angle)]) * dist
             lines.append((x0, y0, angle, dist))
+            ax[2].axline((x0, y0), slope=np.tan(angle + np.pi / 2), color='r')
         print(len(lines))
         
+        plt.tight_layout()
+        plt.show()
 
 
 if __name__ == "__main__":
