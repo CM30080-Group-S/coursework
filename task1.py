9e8859f73acf0d14ed83b92d5f24540d89d68d93
Max Wood <bridges.wood@gmail.com>
2023-02-14 16:18:21 +0000

    Add preprocessing

--- task1.py
+++ task1.py
@@ -1,9 +1,8 @@
 from skimage import io
 from skimage.transform import hough_line, hough_line_peaks
+from skimage.feature import canny
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 def load_images(path):
     """
@@ -15,6 +14,9 @@ def load_images(path):
     images = []
     for file in os.listdir(path):
         if file.endswith(".png"):
+            image = io.imread(path + file, as_gray=True)
+            # Maximise the contrast of the image
+            image = (image - np.min(image)) / (np.max(image) - np.min(image))
             images.append(io.imread(path + file, as_gray=True))
     return images
 
@@ -22,7 +24,7 @@ def hough_transform(image):
     """
     Performs the hough transform on a given image.
     """
-    tested_angles = np.linspace(-np.pi / 2, np.pi / 2, 360)
+    tested_angles = np.linspace(-np.pi / 2, np.pi / 2, 360, endpoint=False)
     h, theta, d = hough_line(image, theta=tested_angles)
     return h, theta, d
     
@@ -41,49 +43,31 @@ def evaluate(predictions, ground_truth):
     # Return the average accuracy
     return np.mean(accuracies)
 
+def preprocess(image):
+    """
+    Preprocesses the image by removing the background.
+    """
+    # Run canny edge detection
+    edges = canny(image, sigma=3, low_threshold=0.1, high_threshold=0.2)
+    # Remove the background
+    image[edges == 0] = 0
+    return image
+
+
 def main(image_path):
     # For each image
     for image in load_images(image_path):
+        # Preprocess the image
+        image = preprocess(image)
+
         # Perform hough transform on the image
         h, theta, d = hough_transform(image)
-
-        fig, axes = plt.subplots(1, 3, figsize=(15, 6))
-        ax = axes.ravel()
-
-        ax[0].imshow(image, cmap=plt.cm.gray)
-        ax[0].set_title('Input image')
-        ax[0].set_axis_off()
-        
-        angle_step = 0.5 * np.diff(theta).mean()
-        dist_step = 0.5 * np.diff(d).mean()
-        bounds = [
-            np.rad2deg(theta[0] - angle_step),
-            np.rad2deg(theta[-1] + angle_step),
-            d[-1] + dist_step,
-            d[0] - dist_step,
-        ]
-        ax[1].imshow(np.log(1 + h), extent=bounds, cmap='gray', aspect='auto')
-        ax[1].set_title('Hough transform')
-        ax[1].set_xlabel('Angles (degrees)')
-        ax[1].set_ylabel('Distance (pixels)')
-        ax[1].axis('image')
-
-        ax[2].imshow(image, cmap=plt.cm.gray)
-        ax[2].set_ylim((image.shape[0], 0))
-        ax[2].set_axis_off()
-        ax[2].set_title('Detected lines')
-
 
         lines = []
         for _, angle, dist in zip(*hough_line_peaks(h, theta, d)):
             (x0, y0) = np.array([np.cos(angle), np.sin(angle)]) * dist
             lines.append((x0, y0, angle, dist))
-            ax[2].axline((x0, y0), slope=np.tan(angle + np.pi / 2), color='r')
         print(len(lines))
-        
-        plt.tight_layout()
-        plt.show()
-
 
 if __name__ == "__main__":
     main("task1/data/")
