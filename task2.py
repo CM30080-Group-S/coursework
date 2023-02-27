5c70c4c9d7d03f88eaf2ed8fc7ab749f6ece2fad
RowanBevan <rb2180@bath.ac.uk>
2023-02-27 14:50:01 +0000

    code review changes

--- task2.py
+++ task2.py
@@ -117,6 +117,9 @@ def generate_templates(image_class, image, levels, angles, verbose=False, use_ca
 
 
 def compute_bounding_box(match,templates):
+    """
+    Returns the bottom left and top right coordinates of the coordinates.
+    """
     emoji, result, index = match
     ij = np.unravel_index(np.argmax(result), result.shape)
     x, y = ij[::-1]
@@ -126,7 +129,7 @@ def compute_bounding_box(match,templates):
 
 def compute_iou(box_1,box_2):
     """
-    Compute the intersection over union of the matching bounding boxes
+    Compute the intersection over union of the matching bounding boxes.
     """
 
     # bl is the bottom left of the given box and tr is the top right
@@ -137,9 +140,11 @@ def compute_iou(box_1,box_2):
 
     intersect = (min(box_1[tr][x], box_2[tr][x]) - max(box_1[bl][x], box_2[bl][x])) * (min(box_1[tr][y], box_2[tr][y]) - max(box_1[bl][y], box_2[bl][y]))
 
+    # If there is no intersection
     if intersect < 0:
         intersect = 0
     union = box_1_area + box_2_area - intersect
+    assert union > 0, "Union must be greater than zero"
     return intersect / union
 
 
@@ -170,6 +175,7 @@ def evaluate(matches, templates, annotations_file):
         emoji = match[0]
         bounding_box = compute_bounding_box(match, templates)
 
+        # Extract the emoji name from the emoji file name
         emoji = emoji.rsplit(".")[0].split("-",1)[1]
 
         # Determine if we have a: true positive, false positive
@@ -191,7 +197,7 @@ def evaluate(matches, templates, annotations_file):
     return true_positives, false_positives, overall_accuracy
 
 
-def main(training_images_path, test_images_path, ground_truths_path, angles, levels, verbose=False):
+def main(training_images_path, test_images_path, ground_truths_path, angles, levels, threshold, verbose=False):
     training_images = load_images(training_images_path)
 
     # Get template variants of each emoji
@@ -214,8 +220,7 @@ def main(training_images_path, test_images_path, ground_truths_path, angles, lev
         elif (Path(annotation_file_path + '.csv')).exists():
             annotation_file_path += '.csv'
         else:
-            print(f'No matching annotations found for test image {key}, continuing to next test image...')
-            continue
+            raise FileNotFoundError(f'No matching annotations found for test image {key}, continuing to next test image...')
 
         # For each pyramid
         matches = []
@@ -233,7 +238,7 @@ def main(training_images_path, test_images_path, ground_truths_path, angles, lev
                 if verbose:
                     print(result.max())
 
-                if result.max() > 0.95:
+                if result.max() > threshold:
                     matches.append((emoji, result, index))
 
         if len(matches) == 0 and verbose:
@@ -268,16 +273,18 @@ if __name__ == "__main__":
     parser.add_argument("ground_truths_path", help="Path to the ground truths")
     parser.add_argument("-a", "--angles", help="Angles between 0 and 360 to use for image rotations (default is 1)", default=1, type=int)
     parser.add_argument("-l", "--levels", help="Gaussian pyramid levels (default is 4)", default=4, type=int)
+    parser.add_argument("-t", "--threshold", help="Threshold required for a match (default is 0.95)", default=0.95, type=float)
     parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
-    training_images_path, test_images_path, ground_truths_path, angles, levels, verbose = (
+    training_images_path, test_images_path, ground_truths_path, angles, levels, threshold, verbose = (
         itemgetter(
             "training_images_path",
             "test_images_path",
             "ground_truths_path",
             "angles",
             "levels",
+            "threshold",
             "verbose"
         )(vars(parser.parse_args()))
     )
 
-    main(training_images_path, test_images_path, ground_truths_path, angles, levels, verbose)
+    main(training_images_path, test_images_path, ground_truths_path, angles, levels, threshold, verbose)
